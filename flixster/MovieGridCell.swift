5ba99b954a65e64c130d784335451bad94778bae
Mike Yiu <mikesun@mikes-macbook-air.local>
2019-02-28 09:07:36 -0500

    changes

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Mike Sun on 2/27/19.
+//  Copyright © 2019 Mike Yiu. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
